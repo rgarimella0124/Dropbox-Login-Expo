4298d7e6f168d19634a21df91168d2b9645ace1e
Ram Simran G <ram@neokred.tech>
2020-09-05 14:17:11 +0530

    first commit

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import App from "./src/app";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default function Dropbox() {
+  return <App />;
 }
